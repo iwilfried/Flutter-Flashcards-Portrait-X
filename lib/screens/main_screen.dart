57483c74dd725efd4a5d5ecb496a3395e26b00cf
Mo'men amin <Momenamiin@gmail.com>
2022-05-15 22:52:32 +0200

    create footer

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -80,105 +80,132 @@ class _MainScreenState extends State<MainScreen> {
         ),
       ),
       body: Scaffold(
-        body: Container(
-          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 5),
-          width: width,
-          height: height,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  colorFilter: ColorFilter.mode(
-                      Theme.of(context).backgroundColor.withOpacity(0.9),
-                      BlendMode.darken),
-                  image: AssetImage(isPortrait
-                      ? "assets/images/backPortrait.png"
-                      : "assets/images/backLandscape.png"),
-                  fit: BoxFit.cover)),
-          child: Padding(
-            padding:
-                const EdgeInsets.only(top: 10, bottom: 10, right: 20, left: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                isPortrait
-                    ? Column(
-                        children: [
-                          Image.asset('assets/images/view.png',
-                              width: 40, height: 40),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          AutoSizeText(
-                            'WELCOME',
-                            style: GoogleFonts.roboto(
-                                textStyle: TextStyle(
-                                    color: Theme.of(context).primaryColor,
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.w300)),
-                          ),
-                        ],
-                      )
-                    : Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+                width: width,
+                height: height,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                            Theme.of(context).backgroundColor.withOpacity(0.9),
+                            BlendMode.darken),
+                        image: AssetImage(isPortrait
+                            ? "assets/images/backPortrait.png"
+                            : "assets/images/backLandscape.png"),
+                        fit: BoxFit.cover)),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10, bottom: 10, right: 20, left: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      isPortrait
+                          ? Column(
+                              children: [
+                                Image.asset('assets/images/view.png',
+                                    width: 40, height: 40),
+                                const SizedBox(
+                                  height: 15,
+                                ),
+                                AutoSizeText(
+                                  'WELCOME',
+                                  style: GoogleFonts.roboto(
+                                      textStyle: TextStyle(
+                                          color: Theme.of(context).primaryColor,
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.w300)),
+                                ),
+                              ],
+                            )
+                          : Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Image.asset('assets/images/view.png',
+                                    width: 40, height: 40),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                AutoSizeText(
+                                  'WELCOME',
+                                  style: GoogleFonts.roboto(
+                                      textStyle: TextStyle(
+                                          color: Theme.of(context).primaryColor,
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.w300)),
+                                ),
+                              ],
+                            ),
+                      const Spacer(),
+                      AutoSizeText(
+                        'General Data Protection Regulation(GDPR) FlashCards',
+                        maxLines: 4,
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.oswald(
+                            textStyle: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 30,
+                        )),
+                      ),
+                      const Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: AutoSizeText(
+                          'Use this flashcards to learn and understand vocabulary, terms and data protection regulations.',
+                          maxLines: 5,
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.oswald(
+                              textStyle: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 18,
+                          )),
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Image.asset('assets/images/view.png',
-                              width: 40, height: 40),
-                          const SizedBox(
-                            width: 20,
-                          ),
-                          AutoSizeText(
-                            'WELCOME',
-                            style: GoogleFonts.roboto(
-                                textStyle: TextStyle(
-                                    color: Theme.of(context).primaryColor,
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.w300)),
-                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: IconButton(
+                                onPressed: () => {},
+                                icon: const Icon(Icons.arrow_forward_sharp,
+                                    size: 40, color: Color(0xffF16623))),
+                          )
                         ],
                       ),
-                const Spacer(),
-                AutoSizeText(
-                  'General Data Protection Regulation(GDPR) FlashCards',
-                  maxLines: 4,
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.oswald(
-                      textStyle: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 30,
-                  )),
-                ),
-                const Spacer(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: AutoSizeText(
-                    'Use this flashcards to learn and understand vocabulary, terms and data protection regulations.',
-                    maxLines: 5,
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.oswald(
-                        textStyle: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontSize: 18,
-                    )),
+                    ],
                   ),
                 ),
-                const Spacer(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: IconButton(
-                          onPressed: () => {},
-                          icon: const Icon(Icons.arrow_forward_sharp,
-                              size: 40, color: Color(0xffF16623))),
-                    )
-                  ],
-                ),
-              ],
+              ),
             ),
-          ),
+            Container(
+              padding: const EdgeInsets.only(
+                left: 20,
+              ),
+              color: Colors.blue,
+              width: double.infinity,
+              height: 45,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: AutoSizeText("General Data Protection Regulation(GDPR)",
+                    maxLines: 1,
+                    style: GoogleFonts.robotoSlab(
+                      textStyle: GoogleFonts.robotoSlab(
+                          textStyle: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500)),
+                    )),
+              ),
+            ),
+          ],
         ),
       ),
     );
