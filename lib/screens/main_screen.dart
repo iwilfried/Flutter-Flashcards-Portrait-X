dfa419afba09d4e26bb661c8079bbeeb4423474c
Mo'men amin <Momenamiin@gmail.com>
2022-05-25 14:14:59 +0200

    creating appbar widget

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
+import '../widgets/appbar.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -12,68 +13,7 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        shape: const Border(top: BorderSide(color: Colors.green, width: 3)),
-        backgroundColor: Theme.of(context).cardColor,
-        centerTitle: false,
-        titleSpacing: 0,
-        shadowColor: Theme.of(context).shadowColor,
-        title: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                flex: 1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 30,
-                      child: Image.asset('assets/images/LogoMaster.png'),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                  flex: 2,
-                  child: Center(
-                    child: Text("Accelerated Learning",
-                        maxLines: 1,
-                        style: GoogleFonts.roboto(
-                            textStyle: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                                fontSize: 24,
-                                fontWeight: FontWeight.w300))),
-                  )),
-              Expanded(
-                flex: 1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    PopupMenuButton<String>(
-                      child: Icon(
-                        Icons.more_vert,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      onSelected: (String value) =>
-                          value == 'Impressum' ? {} : {},
-                      itemBuilder: (BuildContext context) {
-                        return {'Dark mode', 'Impressum'}.map((String choice) {
-                          return PopupMenuItem<String>(
-                            value: choice,
-                            child: Text(choice),
-                          );
-                        }).toList();
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      appBar: FlashCardsAppBar(),
     );
   }
 }
